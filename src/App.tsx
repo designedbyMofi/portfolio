2fbc3c311955862edb2bdb57755cb4f5f8779797
Mofifoluwa Olawuyi <mofolawuyi@gmail.com>
2026-08-27 19:39:13 +0100

    Fit Dysol video to preview frame

--- src/App.tsx
+++ src/App.tsx
@@ -632,7 +632,7 @@ function ProjectPreview({ project, origin, nativeTransition, closing, onClose, o
           {project.video && videoReady ? (
             <video
               ref={(element) => { previewImageRef.current = element; }}
-              className="is-in-view"
+              className={`is-in-view${project.video === '/assets/dysol.mov' ? ' preview__video--cover' : ''}`}
               src={project.video}
               poster={displayedProject.image}
               autoPlay
